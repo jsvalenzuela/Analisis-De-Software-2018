39f920e65639b514e51c5f9d71e4cfbf25991ba7
escurragonzalez <escurra.gonzalez@gmail.com>
2018-06-08 22:22:53 -0300

    Herramienta de testing

---
+++ Herramienta-de-testing/src/principal/LectorProyecto.java
@@ -0,0 +1,33 @@
+package principal;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class LectorProyecto {
+
+	private List<Clase> clases;
+	
+	public LectorProyecto(){
+	}
+	
+	public List<Clase> leerProyecto(File proyecto){
+		this.clases = new ArrayList<Clase>();
+		this.leer(proyecto);
+		return this.clases;
+	}
+
+	private void leer(File archivo) {
+		
+		if(archivo.isDirectory()){
+			for ( File archivoDirectorio : archivo.listFiles()){
+				this.leer(archivoDirectorio);
+			}
+		} else if (archivo.getName().endsWith(".java")) {
+			leerArchivoJava(archivo, this.clases);
+		}
+	}
+
+	protected abstract void leerArchivoJava(File archivo, List<Clase> clasesProyecto);
+	
+}
