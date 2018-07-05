fe5e1d31cf19ff3ecd7a318d70050e23c68c3959
Ajaya, William <royalty2012wla@gmail.com>
2018-07-04 21:43:38 -0300

    Proyecto Algoritmo de Calidad

---
+++ AlgoritmoCalidad/src/constantes/Constantes.java
@@ -0,0 +1,26 @@
+package constantes;
+
+public class Constantes {
+	public static final int BUENA = 2;
+	public static final int REGULAR = 1;
+	public static final int MALA = 0;
+	
+	public static final int PONDERACION_PORTABILIDAD = 2;
+	public static final int PONDERACION_EFICIENCIA = 3;
+	public static final int PONDERACION_USABILIDAD = 4;
+	public static final int PONDERACION_MANTENIBILIDAD = 5;
+	public static final int PONDERACION_FIABILIDAD = 6;
+	public static final int PONDERACION_FUNCIONABILIDAD = 7;
+	
+	public static final int MINIMO_SATISFACTORIO = 51;
+	public static final int MAX_ACEPTABLE = 50;
+	public static final int MIN_ACEPTABLE = 35;
+	public static final int NO_SATISFACTORIO = 34;
+	
+	public static final int POSICION_FUNCIONALIDAD = 0;
+	public static final int POSICION_FIABILIDAD = 1;
+	public static final int POSICION_MANTENIBILIDAD = 2;
+	public static final int POSICION_USABILIDAD = 3;
+	public static final int POSICION_EFICIENCIA = 4;
+	public static final int POSICION_PORTABILIDAD = 5;
+}
--- AlgoritmoCalidad/src/launcher/Lanzador.java
+++ AlgoritmoCalidad/src/launcher/Lanzador.java
@@ -4,15 +4,24 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.awt.event.ActionEvent;
 
 public class Lanzador {
 
 	private JFrame frame;
+	private VentanaTest test;
 
 	/**
 	 * Launch the application.
@@ -21,6 +30,7 @@ public class Lanzador {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 					Lanzador window = new Lanzador();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
@@ -42,14 +52,34 @@ public class Lanzador {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(245, 255, 250));
+		frame.setBounds(100, 100, 640, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 		frame.setTitle("Algoritmo de Calidad - Grupo 5");
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblBienvenido = new JLabel("Bienvenido");
-		lblBienvenido.setFont(new Font("Chandas", Font.BOLD, 20));
-		lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
-		lblBienvenido.setForeground(Color.BLACK);
-		frame.getContentPane().add(lblBienvenido, BorderLayout.NORTH);
+		ImageIcon ImageIcon = new ImageIcon("resource/img/icono.png");
+		Image image = ImageIcon.getImage();
+		frame.setIconImage(image);
+		
+		JLabel lblBienvenidoAlTest = new JLabel("Bienvenido al Test de Calidad");
+		lblBienvenidoAlTest.setForeground(new Color(0, 128, 128));
+		lblBienvenidoAlTest.setFont(new Font("Tahoma", Font.BOLD, 35));
+		lblBienvenidoAlTest.setBounds(41, 41, 534, 113);
+		frame.getContentPane().add(lblBienvenidoAlTest);
+		
+		JButton btnIniciar = new JButton("Iniciar");
+		btnIniciar.setForeground(new Color(0, 0, 0));
+		btnIniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				test = new VentanaTest();
+				frame.setVisible(false);
+			}
+		});
+		btnIniciar.setBackground(new Color(0, 128, 128));
+		btnIniciar.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnIniciar.setBounds(204, 218, 170, 61);
+		frame.getContentPane().add(btnIniciar);
 	}
-	
 }
